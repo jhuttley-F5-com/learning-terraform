ea14d4136c96bcaeb1fe83c13563973244588f40
jhuttley-F5-com <72359774+jhuttley-F5-com@users.noreply.github.com>
2024-05-06 10:43:28 +1200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -14,9 +14,9 @@ output "default_vpc_id" {
   value = data.aws_vpc.default.id
 }
 
-#output "instance_public_ip" {
-#  value = aws_instance.
-#}
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
 
 #output "instance_arn" {
 #  value = aws_instance.web.arn
