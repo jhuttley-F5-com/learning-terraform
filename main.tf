69920a58d8985aea43dcb6ba2693438fbc1acda0
jhuttley-F5-com <72359774+jhuttley-F5-com@users.noreply.github.com>
2024-05-05 17:55:59 +1200

    sg

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group_rule" "http_in" {
   from_port= "80"
   to_port= "80"
   protocol = "tcp"
-  cidr_blocks = "[0.0.0.0/0]"
+  cidr_blocks = ""0.0.0.0/0"]
   security_group_id =aws_security_group.my.id
 
 }
