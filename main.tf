b588171e323553b08f1c075ea4e6a68c53ccd056
jhuttley-F5-com <72359774+jhuttley-F5-com@users.noreply.github.com>
2024-05-05 16:48:26 +1200

    change to vpc

--- main.tf
+++ main.tf
@@ -14,11 +14,29 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "tf-example"
   }
 }
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+#resource "aws_instance" "web" {
+#  ami           = data.aws_ami.app_ami.id
+#  instance_type = "t3.nano"
+
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
