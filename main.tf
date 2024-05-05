7c086302da78320fc40815131130171b0811cee6
jhuttley-F5-com <72359774+jhuttley-F5-com@users.noreply.github.com>
2024-05-05 18:10:06 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,14 @@ resource "aws_security_group_rule" "http_in" {
   security_group_id =aws_security_group.my.id
 
 }
-
+resource "aws_security_group_rule" "http_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.my.id
+}
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
