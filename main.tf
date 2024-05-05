1686f3cfb7e15699b4a237cc20ba596d363dc131
jhuttley-F5-com <72359774+jhuttley-F5-com@users.noreply.github.com>
2024-05-06 10:48:55 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,10 @@ resource "aws_security_group" "my" {
   name = "my_sg"
   description = "allow http"
   vpc_id = aws_vpc.my_vpc.id
-
+  ingress_rules=  ["https:443-tcp", "http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules  =  ['all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "http_in" {
