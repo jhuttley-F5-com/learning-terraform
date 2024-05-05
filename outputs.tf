872e7769b7fc0a96a88c6a9f150142f4044f5e59
jhuttley-F5-com <72359774+jhuttley-F5-com@users.noreply.github.com>
2024-05-05 16:50:31 +1200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,10 @@
-output "instance_ami" {
-  value = aws_instance.web.ami
-}
+#output "instance_ami" {
+#  value = aws_instance.web.ami
+#}
 
+output "pvc_id" {
+  value 
+}
 #output "instance_arn" {
 #  value = aws_instance.web.arn
 #}
